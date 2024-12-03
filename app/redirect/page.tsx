d1c6b8b1b6416d6e084d39f3503cb6007e5ed60b
donubu <magc.cl@gmail.com>
2024-12-03 17:12:18 -0300

    Update redirection logic to fetch original URL
    This commit enhances the redirection logic by querying the `qr_tracking` table to fetch the most recent original URL entry. It ensures that the redirection uses the original URL if available, otherwise defaults to the provided URL. Additionally, it maintains error handling around the scan count increment and original URL fetching processes.

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,29 +1,42 @@
-import { redirect } from 'next/navigation';
-import { supabase } from '@/lib/supabase';
+import {redirect} from 'next/navigation';
+import {supabase} from '@/lib/supabase';
+
+export const dynamic = 'force-dynamic';
 
 export default async function RedirectPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
+                                               searchParams,
+                                           }: {
+    searchParams: { [key: string]: string | undefined };
 }) {
-  const { id, url } = searchParams;
+    const {id, url} = searchParams;
 
-  if (!id || !url) {
-    redirect('/');
-  }
-
-  try {
-    const { error } = await supabase
-      .rpc('increment_scans', { 
-        row_id: id 
-      });
-
-    if (error) {
-      console.error('Error updating scan count:', error);
+    if (!id || !url) {
+        redirect('/');
     }
-  } catch (error) {
-    console.error('Error updating scan count:', error);
-  }
 
-  redirect(decodeURIComponent(url));
+    try {
+        const {error} = await supabase
+            .rpc('increment_scans', {
+                row_id: id
+            });
+
+        if (error) {
+            console.error('Error updating scan count:', error);
+        }
+    } catch (error) {
+        console.error('Error updating scan count:', error);
+    }
+
+    const {data, error} = await supabase
+        .from("qr_tracking")
+        .select("original_url")
+        .eq('id', id)
+        .order("created_at", {ascending: false});
+
+    if (error || data.length < 1) {
+        redirect(decodeURIComponent(url));
+    }
+
+    const {original_url} = data[0];
+    redirect(decodeURIComponent(original_url));
 }
